be8e17c276a808b2d61d712982299a402ed5304c
loara <loarabia@hotmail.com>
2017-07-29 19:54:37 -0700

    [BROKEN] Use standard snprintf and vsnprintf
    The code base used custom written versions of snprintf and vsnprintf. As of
    the UCRT that runs against Visual Studio 2015 and Windows 10, both of
    those are implemented to be C99 standards compliant.
    
    Detecting if we're using a recent VC++ compiler and if so, using the UCRT
    functions instead.
    
    This is probably worth attempting to push upstream with appropriate checks
    (against VS2015 aka < version 1900)

--- sc2/src/port.c
+++ sc2/src/port.c
@@ -118,6 +118,7 @@ readdir_r(DIR *dirp, struct dirent *entry, struct dirent **result) {
 // MSVC does not have snprintf() and vsnprintf(). It does have a _snprintf()
 // and _vsnprintf(), but these do not terminate a truncated string as
 // the C standard prescribes.
+#if _MSC_VER < 1910
 int
 snprintf(char *str, size_t size, const char *format, ...)
 {
@@ -141,5 +142,6 @@ vsnprintf(char *str, size_t size, const char *format, va_list args)
 		str[size - 1] = '\0';
 	return result;
 }
+#endif  /* _MSC_VER < 1910 */
 #endif  /* _MSC_VER */
 
